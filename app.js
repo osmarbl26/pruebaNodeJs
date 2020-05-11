b0dcd70d2bc3b2db0aa19a0e4d6aa5ea87f5e3f8
Osmar Bravo <osmarbl26@gmail.com>
2020-05-10 23:15:39 -0400

    Updates

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+//const express = require('express');
+const app = (0, _express.default)();
+const PORT = 3000;
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Hola mundo'
+  });
+});
+app.listen(PORT, () => {
+  console.log("corriendo en el puerto: ", PORT);
+});
